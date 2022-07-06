e623c172a502037fee547e945829116b0de18b86
Your Name <you@example.com>
2022-07-06 05:12:26 -0700

    alx c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void print_to_98(int n)
+{     while(n>=98)
+
+	    {
+
+		            printf("%d, ", n);
+
+			        n-}
+	    while(<=98)
+	    { printf("%d, ", n);				              
+		    ++;
+									           }
+	    printf("\n");
+}
